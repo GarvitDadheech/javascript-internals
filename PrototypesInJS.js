1580721baef2197a3a3d6fbbce729f15bb8c6a7e
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-09 23:14:11 +0530

    new keyword

--- PrototypesInJS.js
+++ PrototypesInJS.js
@@ -17,5 +17,8 @@ createUser.prototype.print = function() {
 //const user = createUser("Garvit",20);
 //the above code will give error as we are not using the new keyword.
 // so new keyword tells the functions that there is a method in createUser, so new keyword knows 
+// the new keyword initiates the new object and then newly created object gets linked with prototype property of constructor function.
+// this means that now it has access to properties and methods defined on constructor's prototype.
 const user = new createUser("Garvit",20);
 user.print();
+
